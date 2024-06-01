d6226ce2c370bb13b1cf11028915a6c2f8ec4772
Astro <85232708+AbhishekSaini04@users.noreply.github.com>
2024-06-01 23:29:12 +0530

    overall improvement in the logic of disconnect of text chst and video chat

--- index.js
+++ index.js
@@ -26,7 +26,17 @@ let noneUser = textChatUsersArray.indexOf("");
 if (noneUser == 0) {
   textChatUsersArray.splice(noneUser);
 }
-let videoChatUsersArray;
+
+let textChatConn = [];
+
+let videoChatUsersArray = [""];
+let videoNoneUser = videoChatUsersArray.indexOf("");
+
+if (noneUser == 0) {
+  videoChatUsersArray.splice(videoNoneUser);
+}
+
+let videoChatConn = [];
 
 // helper functions for logs
 function fillZero(val) {
@@ -51,27 +61,84 @@ const io = new Server(server);
 // socket io on connection
 io.on("connection", (socket) => {
   //state of app
-  console.log(timestamp(), "Connected:", socket.id);
+  socket.on("videoChat", (condition) => {
+    console.log(timestamp(), "Video Connected:", socket.id);
 
-  textChatUsersArray.push(socket.id);
-  console.log(`Total users=${textChatUsersArray.length}`);
-  io.emit("ttl", textChatUsersArray.length);
+    videoChatUsersArray.push(socket.id);
+    console.log(
+      `Total users=${videoChatUsersArray.length + textChatUsersArray.length}`
+    );
+    io.emit("ttl", videoChatUsersArray.length + videoChatUsersArray.length);
 
-  //========checking if any strangers are availbale or not===========
-  const user = textChatUsersArray.indexOf(socket.id);
+    //========checking if any strangers for video are availbale or not===========
+    const user = videoChatUsersArray.indexOf(socket.id);
 
-  if (user % 2 !== 0 && textChatUsersArray[user - 1]) {
-    socket.to(textChatUsersArray[user - 1]).emit("strangerConnection", true);
-    socket.to(textChatUsersArray[user]).emit("strangerConnection", true);
-  }
-
-  socket.on("connectionReq", (condition) => {
-    if (condition) {
-      const user = textChatUsersArray.indexOf(socket.id);
-      socket.to(textChatUsersArray[user + 1]).emit("strangerConnection", true);
+    if (user % 2 !== 0 && videoChatUsersArray[user - 1]) {
+      socket.to(videoChatUsersArray[user - 1]).emit("strangerConnection", true);
+      socket.to(videoChatUsersArray[user]).emit("strangerConnection", true);
+      videoChatConn.push({
+        first: videoChatUsersArray[user],
+        second: videoChatUsersArray[user - 1],
+      });
     }
+
+    socket.on("connectionReq", (condition) => {
+      if (condition) {
+        const user = videoChatUsersArray.indexOf(socket.id);
+        socket
+          .to(videoChatUsersArray[user + 1])
+          .emit("strangerConnection", true);
+      }
+    });
+    console.log("VIdeo Chat Connections", videoChatConn);
+    console.log("VIdeo ", videoChatUsersArray);
+    console.log(
+      "Text Chat Users",
+      textChatUsersArray.length,
+      "  Video Chat Users",
+      videoChatUsersArray.length
+    );
   });
 
+  socket.on("textChat", (condition) => {
+    console.log(timestamp(), "Text Connected:", socket.id);
+    textChatUsersArray.push(socket.id);
+    console.log(
+      `Total users=${textChatUsersArray.length + videoChatUsersArray.length}`
+    );
+    io.emit("ttl", textChatUsersArray.length + videoChatUsersArray.length);
+
+    //========checking if any strangers are availbale or not===========
+    const user = textChatUsersArray.indexOf(socket.id);
+
+    if (user % 2 !== 0 && textChatUsersArray[user - 1]) {
+      socket.to(textChatUsersArray[user - 1]).emit("strangerConnection", true);
+      socket.to(textChatUsersArray[user]).emit("strangerConnection", true);
+      textChatConn.push({
+        first: textChatUsersArray[user],
+        second: textChatUsersArray[user - 1],
+      });
+    }
+
+    socket.on("connectionReq", (condition) => {
+      if (condition) {
+        const user = textChatUsersArray.indexOf(socket.id);
+        socket
+          .to(textChatUsersArray[user + 1])
+          .emit("strangerConnection", true);
+      }
+    });
+    console.log("Text Chat Connections", textChatConn);
+    console.log("Text  ", textChatUsersArray);
+    console.log(
+      "Text Chat Users",
+      textChatUsersArray.length,
+      "  Video Chat Users",
+      videoChatUsersArray.length
+    );
+  });
+
+  // sending text
   socket.on("user-message", (message) => {
     //================new logic================
     const user = textChatUsersArray.indexOf(socket.id);
@@ -81,66 +148,159 @@ io.on("connection", (socket) => {
     } else {
       socket.to(textChatUsersArray[user - 1]).emit("message", message);
     }
-    //================old logic================
-    // if(socket.id===textChatUsersArray[0]){
-
-    //     console.log("User Message:", message);
-    //     // sends data to clients
-
-    //     socket.to(textChatUsersArray[1]).emit("message", message);
-    //   }
+   
   });
-  //Message emit
+
+  //================ new for video /// sending video========
+  socket.on("stream", (stream) => {
+    //================new logic================
+    const user = videoChatUsersArray.indexOf(socket.id);
+    console.log(typeof stream);
+
+    if (user % 2 === 0) {
+      socket.to(videoChatUsersArray[user + 1]).emit("strangerStream", stream);
+    } else {
+      socket.to(videoChatUsersArray[user - 1]).emit("strangerStream", stream);
+    }
+  });
+  socket.on("disconnecting", (condition) => {
+    console.log("I am The Condition=========", condition);
+  });
 
   // socket  on disconnect
   socket.on("disconnecting", (reason) => {
     //===========new logic===========
-    const user = textChatUsersArray.indexOf(socket.id);
-    console.log(user, " Disconnected");
-    if (user % 2 === 0) {
-      socket.to(textChatUsersArray[user + 1]).emit("disconnectedUser", true);
-    } else {
-      socket.to(textChatUsersArray[user - 1]).emit("disconnectedUser", true);
+    console.log(
+      "++++++++++++ ON Disconnect TEXT",
+      textChatUsersArray.indexOf(socket.id)
+    );
+    console.log(
+      "++++++++++++ ON Disconnect VIDEO",
+      videoChatUsersArray.indexOf(socket.id)
+    );
+    // TEXT CHAT DISCONNECTION
+    if (textChatUsersArray.indexOf(socket.id) !== -1) {
+      
+      console.log("TEXT Connections:", textChatConn);
+
+      textChatConn.forEach((element) => {
+        console.log("Im in foreach before disconnected");
+        if (element.first === socket.id) {
+          socket.to(element.second).emit("disconnectedUser", true);
+
+          // removing item from Array
+          console.log(timestamp(), "Disconnected:", socket.id);
+          //removing connection
+          textChatConn = textChatConn.filter((item) => {
+            return item !== element;
+          });
+          textChatUsersArray = textChatUsersArray.filter((item) => {
+            return item !== element.second;
+          });
+          textChatUsersArray = textChatUsersArray.filter((item) => {
+            return item !== element.first;
+          });
+        }
+        if (element.second === socket.id) {
+          socket.to(element.first).emit("disconnectedUser", true);
+          // removing item from Array
+          console.log(timestamp(), "Disconnected:", socket.id);
+
+          //removing connection
+          textChatConn = textChatConn.filter((item) => {
+            return item !== element;
+          });
+
+          textChatUsersArray = textChatUsersArray.filter((item) => {
+            return item !== element.second;
+          });
+          textChatUsersArray = textChatUsersArray.filter((item) => {
+            return item !== element.first;
+          });
+        }
+      });
+
+      if (textChatUsersArray.indexOf(socket.id) !== -1) {
+        console.log(timestamp(), "Finally Disconnected:", socket.id);
+        textChatUsersArray = textChatUsersArray.filter((item) => {
+          return item !== socket.id;
+        });
+      }
+
+      console.log("Text Client Available:", textChatUsersArray);
     }
-    //============old logic==========
-    // if(socket.id===textChatUsersArray[0]){
+     else {
+            console.log("VIDEO Connections:", videoChatConn);
 
-    //   // sends data to clients
-    //  console.log("Disconnected");
-    //   socket.to(textChatUsersArray[1]).emit("disconnectedUser", true);
-    // }
+      videoChatConn.forEach((element) => {
+        console.log("Im in foreach before disconnected");
+        if (element.first === socket.id) {
+          socket.to(element.second).emit("disconnectedUser", true);
 
-    console.log(textChatUsersArray);
-    // console.log(reason);
-    console.log(timestamp(), "Disconnected:", socket.id);
-    textChatUsersArray = textChatUsersArray.filter((item) => {
-      return item !== socket.id;
-    });
+          // removing item from Array
+          console.log(timestamp(), "Disconnected:", socket.id);
+          //removing connection
+          videoChatConn = videoChatConn.filter((item) => {
+            return item !== element;
+          });
+          videoChatUsersArray = videoChatUsersArray.filter((item) => {
+            return item !== element.second;
+          });
+          videoChatUsersArray = videoChatUsersArray.filter((item) => {
+            return item !== element.first;
+          });
+        }
+        if (element.second === socket.id) {
+          socket.to(element.first).emit("disconnectedUser", true);
+          // removing item from Array
+          console.log(timestamp(), "Disconnected:", socket.id);
 
-    console.log(textChatUsersArray);
+          //removing connection
+          videoChatConn = videoChatConn.filter((item) => {
+            return item !== element;
+          });
 
-    console.log(`Total users=${textChatUsersArray.length}`);
-    io.emit("ttl", textChatUsersArray.length);
+          videoChatUsersArray = videoChatUsersArray.filter((item) => {
+            return item !== element.second;
+          });
+          videoChatUsersArray = videoChatUsersArray.filter((item) => {
+            return item !== element.first;
+          });
+        }
+      });
+
+      if (videoChatUsersArray.indexOf(socket.id) !== -1) {
+        console.log(timestamp(), "Finally Disconnected:", socket.id);
+        videoChatUsersArray = videoChatUsersArray.filter((item) => {
+          return item !== socket.id;
+        });
+      }
+
+      console.log("Video Client Available:", videoChatUsersArray);
+    }
+
+
+    console.log(
+      "Text Chat Users",
+      textChatUsersArray.length,
+      "  Video Chat Users",
+      videoChatUsersArray.length
+    );
+    console.log(
+      `Total users=${textChatUsersArray.length + videoChatUsersArray.length}`
+    );
+    io.emit("ttl", textChatUsersArray.length + videoChatUsersArray.length);
   });
 
+  // old for video-chat
+  // socket.on("stream",(stream)=>{
+  //   // console.log("video data:",stream);
+  //   socket.broadcast.emit("strangerStream",stream);
 
-  // for video-chat
-  socket.on("stream",(stream)=>{
-    // console.log("video data:",stream);
-    socket.broadcast.emit("strangerStream",stream);
-
-  })
+  // })
 });
 
 console.log("Current Time:", timestamp());
-
-
-
-
-
-
-
-
 
 // ------------all routes-------------
 
