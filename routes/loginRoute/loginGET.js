e7ba9907507862dd64b4e9c4744c8ae2b86f346b
Astro <85232708+AbhishekSaini04@users.noreply.github.com>
2024-06-08 21:33:13 +0530

    Add files via upload

--- routes/loginRoute/loginGET.js
+++ routes/loginRoute/loginGET.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 router.route("/").get( (req, res) => {
-    res.render("../views/routeNotFound");
+    res.render("../views/login");
     res.status(200);
   });
 
